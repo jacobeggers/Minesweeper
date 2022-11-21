76ff341b0cc841d4750ee9dcd744c77289479251
jacobeggers <116228370+jacobeggers@users.noreply.github.com>
2022-11-21 09:30:28 -0800

    fixed mine overlap detection (hopefully)

--- Frame.java
+++ Frame.java
@@ -6,19 +6,35 @@ public class Frame extends JFrame implements MouseListener {
 	
 	Panel panel = new Panel();
 	
+	private boolean testForOverlap(int[] bx, int[] by) {
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				if (i != j && bx[i] == bx[j] && by[i] == by[j]) {
+					String testX = "";
+					String testY = "";
+					for (int ii = 0; ii < 10; ii++) {
+						testX += bx[ii];
+						testY += by[ii];
+					}
+					System.out.println(String.format("(%s)", testX));
+					System.out.println(String.format("(%s)", testY));
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
 	public Frame() {
 		System.out.println();
 		Bomb bomb = new Bomb(10, 8, 8);
 		int[] bombX = bomb.placeBombsX();
 		int[] bombY = bomb.placeBombsY();
-		for (int i = 0; i < 10; i++) {
-			for (int j = 0; j < 10; j++) {
-				if (i != j && bombX[i] == bombX[j] && bombY[i] == bombY[j]) {
-					System.out.println("Mines Overlapped");
-					bombX = bomb.placeBombsX();
-					bombY = bomb.placeBombsY();
-				}
-			}
+		
+		while (testForOverlap(bombX, bombY) == true) {
+			System.out.println("Mines Overlapped");
+			bombX = bomb.placeBombsX();
+			bombY = bomb.placeBombsY();
 		}
 		
 		String testX = "";
@@ -34,6 +50,7 @@ public class Frame extends JFrame implements MouseListener {
 		System.out.println(Board.drawBoard());
 
 		this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+		this.setTitle("Minesweeper");
 		this.add(panel);
 		this.pack();
 		this.addMouseListener(this);
